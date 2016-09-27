7b878c55a9e76be1ff4d72d4143ace055683af50
123016 <766812700@qq.com>
2016-09-27 09:17:22 +0800

    finsh the list and toast

--- app/src/main/java/com/magic/udacitylist/MainActivity.java
+++ app/src/main/java/com/magic/udacitylist/MainActivity.java
@@ -2,6 +2,9 @@ package com.magic.udacitylist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void onButtonClick(View v){
+        Button button = (Button) v;
+        String name = button.getText().toString();
+        Toast.makeText(getApplicationContext(),name,Toast.LENGTH_SHORT).show();
     }
 }
